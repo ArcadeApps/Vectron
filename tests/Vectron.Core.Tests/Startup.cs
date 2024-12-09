0ebc39854ab78ac1ec0d60ea7fabcdc2a4a996c9
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2024-12-09 17:40:36 +0100

    chore: checkin Unit tests

---
+++ tests/Vectron.Core.Tests/Startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using Vectron.Core.ServerDiscovery;
+
+namespace Vectron.Core.Tests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddServerDiscovery();
+    }
+}
---
+++ tests/Vectron.Core.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Vectron.Core.Authentication;
+
+namespace Vectron.Core.Tests;
+
+public class UnitTest1
+{
+    private readonly IRegisterApi _registerApi;
+
+    public UnitTest1(IRegisterApi registerApi)
+    {
+        _registerApi = registerApi;
+    }
+    
+    [Fact]
+    public async Task RegisterAPI_ValidateEmail()
+    {
+        var result = await _registerApi.ValidateEmailAsync("secret", "test@test.com");
+        Assert.True(result.IsSuccess);
+    }
+}
